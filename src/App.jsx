9163962a0929ef0da0acc5f54291c807f0168389
LUDEBRADevelopment <ludebradevelopment@gmail.com>
2024-03-07 19:24:56 -0500

    Overview Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Dielectric } from './labs/dielectric/Dielectric.jsx'
+import { Prueba } from './pages/overview/Prueba.jsx'
 
 function App() {
 
   return (
     <>
-      <Dielectric />
+      <Prueba />
+      {/* <Dielectric /> */}
     </>
   )
 }
---
+++ src/pages/overview/Prueba.jsx
@@ -0,0 +1,10 @@
+import './Prueba.css'
+export function Prueba() {
+    return(
+        <div className='container'>
+            <h1>Dielectric</h1>
+        </div>
+    )
+
+
+}
