901d0fe896144b673a8cad7111cd0599d4e4289c
mibichoandafugao <augustohenao1234@gmail.com>
2024-04-20 21:00:03 -0500

    Login Prototype Finished

---
+++ src/components/login/FooterLogin.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logoludebra from '../../assets/LUDEBRA LOGO.png'
+import '../styles/login/FooterLogin.css'
+
+export function FooterLogin() {
+  return (
+    <div>
+      <section>
+        <footer>
+          <p class='copyright'>© Copyright LUDEBRA DEVELOPMENT - 2024</p>
+          <img src={logoludebra} className='logo2' />
+        </footer>
+      </section>
+    </div>
+  )
+}
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Login.css'
-import { Footer } from '../../components/overview/Footer.jsx'
+import { FooterLogin } from '../../components/login/FooterLogin'
 import logo from '../../assets/logo without background.png'
 
 export function Login() {
@@ -15,7 +15,7 @@ export function Login() {
           <div className='login-inputs'>
             <input
               type='text'
-              placeholder='Email o número de teléfono'
+              placeholder='Email'
               id='email-user'
               name='email-user_be'
               required
@@ -28,10 +28,6 @@ export function Login() {
               required
             />
             <button className='btn-iniciar-sesion'>Iniciar sesión</button>
-            <p className='o'>O</p>
-            <button className='btn-codigo'>
-              Usar un código de inicio de sesión
-            </button>
             <a href='#'>¿Olvidaste la contraseña?</a>
           </div>
 
@@ -39,23 +35,10 @@ export function Login() {
             <input type='checkbox' name='#' id='#' />
             <p>Recuérdame</p>
           </div>
-          <p className='primeravez'>
-            ¿Primera vez en Netflix?
-            <a href='' className='pveza'>
-              Suscríbete ahora.
-            </a>
-          </p>
-          <p className='captcha'>
-            Esta página está protegida por Google reCAPTCHA para comprobar que
-            no eres un robot.{' '}
-            <a href='' class='info'>
-              Más info.
-            </a>
-          </p>
         </div>
       </main>
 
-      <Footer />
+      <FooterLogin />
     </div>
   )
 }
