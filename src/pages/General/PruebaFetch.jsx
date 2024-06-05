419eca74e66fbae6a10241b3b71025bb574b785e
mibichoandafugao <augustohenao1234@gmail.com>
2024-06-05 18:26:28 -0500

    Minor Changes

---
+++ src/pages/General/PruebaFetch.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { defaultUrlPath } from '../../models/GlobalVars'
+import { useInfoUsersStore } from '../../store/infoUsersStore'
+
+export function PruebaFetch() {
+  const userToken = useInfoUsersStore(state => state.token)
+
+  useEffect(() => {
+    const values = {
+      Id_course: '1',
+      Id_task: 3,
+    }
+    fetch(`${defaultUrlPath}/users/info/task`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify(values),
+    })
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, [])
+
+  return (
+    <div>
+      <h1>PruebaFetch</h1>
+    </div>
+  )
+}
